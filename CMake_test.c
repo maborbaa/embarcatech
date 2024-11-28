057526690fc7a8047b12f21fc5c96116b2869a94
maborbaa <marciobar@gmail.com>
2024-11-28 15:21:04 -0300

    testar ambiente

---
+++ CMake_test.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Simulação de Raspberry Pi Pico\n");
+    while (1) {
+        // Loop infinito
+    }
+    return 0;
+}
---
+++ build/CMakeFiles/simulated_pico.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simulated_pico.dir/main.c.o"
+  "CMakeFiles/simulated_pico.dir/main.c.o.d"
+  "simulated_pico"
+  "simulated_pico.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/simulated_pico.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
